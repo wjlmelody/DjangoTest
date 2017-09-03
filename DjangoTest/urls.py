c967e6a9d3df78d01a02c4d85e2af8e503a0e465
wjl <wjlmelody@foxmail.com>
2017-09-03 19:25:05 +0800

    初步学习完毕

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import *
 from . import view, testdb, search, search2
+from django.contrib import admin
  
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^hello$', view.hello),
     url(r'^testdb$', testdb.testdb),
     url(r'^search-form$', search.search_form),
