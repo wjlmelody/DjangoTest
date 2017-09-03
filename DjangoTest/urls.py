5abc961ac8c1166ce8a04e0eaec6128f1a7c6818
wjl <wjlmelody@foxmail.com>
2017-09-03 18:54:09 +0800

    表单学习完毕

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import *
-from . import view,testdb
+from . import view, testdb, search, search2
  
 urlpatterns = [
     url(r'^hello$', view.hello),
     url(r'^testdb$', testdb.testdb),
+    url(r'^search-form$', search.search_form),
+    url(r'^search$', search.search),
+    url(r'^search-post$', search2.search_post),
 ]
--- DjangoTest/view.py
+++ DjangoTest/view.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    context          = {}
+    context = {}
     context['hello'] = 'Hello World!'
     return render(request, 'hello.html', context)
