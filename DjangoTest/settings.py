84f986722fe47c1e9d12b115c6db23874e199d0f
wjl <wjlmelody@foxmail.com>
2017-09-03 18:35:12 +0800

    数据库连接界面添加数据成功

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -30,14 +30,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'DjangoTest',               # 添加此项
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'DjangoTest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 或者使用 mysql.connector.django
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'heheda',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
